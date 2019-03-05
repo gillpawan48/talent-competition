c4ed025e351a758c53670ba807982edaf3dd1ec1
gillpawan48 <gillpawan48@yahoo.co.nz>
2019-03-05 17:32:47 +1300

    update project

--- App/Talent.App.WebApp/wwwroot/js/react/ReactScripts/Employer/ManageJob/ManageJob.jsx
+++ App/Talent.App.WebApp/wwwroot/js/react/ReactScripts/Employer/ManageJob/ManageJob.jsx
@@ -5,7 +5,7 @@ import LoggedInBanner from '../../Layout/Banner/LoggedInBanner.jsx';
 import { LoggedInNavigation } from '../../Layout/LoggedInNavigation.jsx';
 import { JobSummaryCard } from './JobSummaryCard.jsx';
 import { BodyWrapper, loaderData } from '../../Layout/BodyWrapper.jsx';
-import { Pagination, Icon, Dropdown, Checkbox, Accordion, Form, Segment } from 'semantic-ui-react';
+import {Card, Button, Pagination, Icon, Dropdown, Checkbox, Accordion, Form, Segment } from 'semantic-ui-react';
 
 export default class ManageJob extends React.Component {
     constructor(props) {
@@ -15,7 +15,13 @@ export default class ManageJob extends React.Component {
         loader.allowedUsers.push("Recruiter");
         //console.log(loader)
         this.state = {
-            loadJobs: [],
+            Title: "List of Jobs",
+            loadJobs: [{
+                title: '',
+                summary: '',
+                location: {
+                    country: '', city: ''
+                }}],
             loaderData: loader,
             activePage: 1,
             sortBy: {
@@ -36,17 +42,17 @@ export default class ManageJob extends React.Component {
         this.loadNewData = this.loadNewData.bind(this);
         //your functions go here
     };
-
     init() {
-        let loaderData = TalentUtil.deepCopy(this.state.loaderData)
-        loaderData.isLoading = false;
-        this.setState({ loaderData });//comment this
+        //let loaderData = TalentUtil.deepCopy(this.state.loaderData)
+        //loaderData.isLoading = false;
+        //this.setState({ loaderData });//comment this
 
         //set loaderData.isLoading to false after getting data
-        //this.loadData(() =>
-        //    this.setState({ loaderData })
-        //)
-        
+        this.loadData(() =>
+            this.setState({ loaderData }),
+            loaderData.isLoading = false
+        )
+
         //console.log(this.state.loaderData)
     }
 
@@ -54,31 +60,140 @@ export default class ManageJob extends React.Component {
         this.init();
     };
 
-    loadData(callback) {
-        var link = 'http://localhost:51689/listing/listing/getSortedEmployerJobs';
-        var cookies = Cookies.get('talentAuthToken');
-       // your ajax call and other logic goes here
+loadData(callback) {
+    //var link = 'http://localhost:51689/listing/listing/getSortedEmployerJobs';
+    var cookies = Cookies.get('talentAuthToken');
+    // your ajax call and other logic goes here
+
+    $.ajax({
+        url: 'http://localhost:51689/listing/listing/getSortedEmployerJobs',
+        headers: {
+            'Authorization': 'Bearer ' + cookies,
+            'Content-Type': 'application/json'
+        },
+        type: "GET",
+        contentType: "application/json",
+        data: {
+            activePage: this.state.activePage,
+            sortbyDate: this.state.sortBy.date,
+            showActive: this.state.filter.showActive,
+            showClosed: this.state.filter.showClosed,
+            showDraft: this.state.filter.showDraft,
+            showExpired: this.state.filter.showExpired,
+            showUnexpired: this.state.filter.showUnexpired
+
+        },
+        dataType: "json",
+        success: function (jobs) {
+            if (jobs.myJobs) {
+                this.state.loadJobs = jobs.myJobs;
+                //console.log("result", this.state.loadJobs)
+                callback();
+            }
+            }.bind(this),
+            error: function (res) {
+                console.log("error");
+                callback();
+            }
+        })
     }
 
     loadNewData(data) {
-        var loader = this.state.loaderData;
-        loader.isLoading = true;
-        data[loaderData] = loader;
-        this.setState(data, () => {
-            this.loadData(() => {
-                loader.isLoading = false;
-                this.setState({
-                    loadData: loader
-                })
-            })
-        });
-    }
 
-    render() {
-        return (
-            <BodyWrapper reload={this.init} loaderData={this.state.loaderData}>
-               <div className ="ui container">Your table goes here</div>
-            </BodyWrapper>
-        )
-    }
 }
+
+loadNewData(data) {
+    var loader = this.state.loaderData;
+    loader.isLoading = true;
+    data[loaderData] = loader;
+    this.setState(data, () => {
+        this.loadData(() => {
+            loader.isLoading = false;
+            this.setState({
+                loadData: loader
+            })
+        })
+    });
+}
+
+render() {
+    let jobIndividualItems = this.state.loadJobs;
+    console.log("jobitems", jobIndividualItems)
+    let jobCardItems = null;
+    if (jobIndividualItems != null) {
+
+        jobCardItems = jobIndividualItems.map(items =>
+            <Card key={items._id}>
+                <Card.Content>
+                    <Card.Header>{items.title}</Card.Header>
+                    <a className="ui black right ribbon label"><i className="user icon"></i></a>
+                    <br />
+                    <Card.Meta>{items.location.city},{items.location.country} </Card.Meta>
+                    <Card.Description>{items.summary}</Card.Description>
+                </Card.Content>
+                <Card.Content extra>
+                    <Button color="red" floated="left" size="mini">Expired</Button>
+                    <Button.Group floated="right" size="mini">
+                        <Button className="ui blue basic"><Icon name="ban" />Close</Button>
+                        <Button className="ui blue basic"><Icon name="edit" />Edit</Button>
+                        <Button className="ui blue basic"><Icon name="copy" />Copy</Button>
+                    </Button.Group>
+                </Card.Content>
+            </Card>
+        )
+
+    }
+    console.log(jobIndividualItems);
+
+    return (
+        <BodyWrapper reload={this.init} loaderData={this.state.loaderData}>
+            <section className="page-body">
+                <div className="ui container">
+                    <h1>{this.state.Title}</h1>
+                    <i className="filter icon"></i> Filter:
+                    <i className="dropdown icon"></i>
+                    <div className="ui simple dropdown item" >
+                        <i className="calendar alternate outline icon"></i>
+                        Sort By Date:
+                               <i className="dropdown icon"></i>
+
+                    </div>
+                    <br />
+                    <br />
+                    {/*jobcard goes here*/}
+                    <div className="ui three cards">
+                        {jobCardItems}
+                    </div>
+                    {/*jobcard goes here*/}
+                    {/*Pagination goes here*/}
+                    <br />
+                    <br />
+                    <br />
+                    <div align="center">
+                        <div className=" ui pagination menu" >
+                            <a className="item">
+                                <i className="left angle double chevron icon"></i>
+                            </a>
+                            <a className="item">
+                                <i className="left angle chevron icon"></i>
+                            </a>
+
+                            <a className="item">
+                                <i className="right angle chevron icon"></i>
+                            </a>
+
+                            <a className="item">
+                                <i className="right angle double chevron icon"></i>
+                            </a>
+
+
+                        </div>
+                    </div>
+                    {/*Pagination goes here*/}
+
+                </div>
+            </section>
+        </BodyWrapper>
+    )
+}
+} 
